5173da26b937f003f91672fb007b2d24765457e1
Jordan Harband <ljharb@gmail.com>
2017-12-04 17:32:28 -0800

    Add `forEachVariation`

---
+++ forEachVariation.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const getComponentName = function getComponentName(C) {
+  if (typeof C === 'string') {
+    return C;
+  }
+  if (typeof C === 'function') {
+    return C.displayName || C.name;
+  }
+  return null;
+};
+
+module.exports = function forEachVariation(descriptor, consumer, callback) {
+  const {
+    component,
+    createdAt: rootCreatedAt,
+    usage,
+    options: { [consumer]: rootOptions = {} } = {},
+    variations,
+  } = descriptor;
+
+  // this consumer is disabled
+  if (!rootOptions || rootOptions.disabled) { return; }
+
+  variations.forEach((variation) => {
+    const {
+      title,
+      createdAt: variationCreatedAt,
+      options: { [consumer]: variationOptions = {} } = {},
+      render,
+    } = variation;
+
+    // this consumer is disabled
+    if (!variationOptions || variationOptions.disabled) { return; }
+
+    const componentName = Array.isArray(component) ? component.map(x => getComponentName(x)) : getComponentName(component);
+
+
+    const options = Object.assign({}, rootOptions, variationOptions);
+    const createdAt = variationCreatedAt || rootCreatedAt;
+
+    const newVariation = Object.assign(
+      {
+        componentName,
+        title,
+        component,
+        usage,
+        options,
+        rootOptions,
+      },
+      createdAt && { createdAt },
+      variation,
+      { options },
+    );
+    callback(newVariation);
+  });
+};
