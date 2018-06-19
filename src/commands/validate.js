55e6b8cd88e8e94b133a865924365b0480a32e35
Jordan Harband <ljharb@gmail.com>
2018-06-18 21:51:29 -0700

    [Refactor] `validate`: make "get overall errors" logic reusable

--- src/commands/validate.js
+++ src/commands/validate.js
@@ -2,8 +2,38 @@
 
 const chalk = require('chalk');
 const has = require('has');
+const yargs = require('yargs');
 
 const getValidationErrors = require('../helpers/getValidationErrors');
+
+function getOverallErrors(variations, components, log, warn, error) {
+  if (variations.length === 0) {
+    error(chalk.red(chalk.bold('No Variation Providers found.')));
+    return 1;
+  }
+
+  if (components.length === 0) {
+    error(chalk.red(chalk.bold('No Components found.')));
+    return 1;
+  }
+
+  log(chalk.blue(`${chalk.bold(components.length)} Components found...`));
+  log(chalk.green(`${chalk.bold(variations.length)} Variation Providers found...`));
+
+  if (components.length < variations.length) {
+    error(chalk.red(chalk.bold('Found fewer Components than Variation Providers.')));
+    return 1;
+  }
+
+  const errors = getValidationErrors(variations);
+
+  if (errors.length > 0) {
+    errors.forEach((e) => { warn(e); });
+    return errors.length;
+  }
+  log(chalk.green(`${chalk.bold('Success!')} All Variation Providers appear to be valid.`));
+  return 0;
+}
 
 exports.command = 'validate [variations]';
 exports.desc = 'validate Variation Providers';
@@ -13,36 +43,19 @@ exports.builder = (yargs) => {
 
   const { project, projects } = yargs.argv;
   if (project && !has(projects, project)) {
-    throw chalk.red(`Project "${project}" missing from projects config`);
+    throw chalk.red(`Project "${project}" missing from “projects” config`);
   }
   if (project) {
     yargs.config(projects[project]);
   }
 };
-exports.handler = (argv) => {
-  if (argv.variations.length === 0) {
-    console.error(chalk.red(chalk.bold('No Variation Providers found.')));
-    process.exit(1);
-  }
-
-  if (argv.components.length === 0) {
-    console.error(chalk.red(chalk.bold('No Components found.')));
-    process.exit(2);
-  }
-
-  console.log(chalk.blue(`${chalk.bold(argv.components.length)} Components found...`));
-  console.log(chalk.green(`${chalk.bold(argv.variations.length)} Variation Providers found...`));
-
-  if (argv.components.length < argv.variations.length) {
-    console.error(chalk.red(chalk.bold('Found fewer Components than Variation Providers.')));
-    process.exit(3);
-  }
-
-  const errors = getValidationErrors(argv.variations);
-
-  if (errors.length > 0) {
-    errors.forEach((error) => { console.warn(error); });
-    process.exit(errors.length);
-  }
-  console.log(chalk.green(`${chalk.bold('Success!')} All Variation Providers appear to be valid.`));
+exports.handler = ({ variations, components }) => {
+  const exitCode = getOverallErrors(
+    variations,
+    components,
+    x => console.log(x),
+    x => console.warn(x),
+    x => console.error(x),
+  );
+  process.exit(exitCode);
 };
