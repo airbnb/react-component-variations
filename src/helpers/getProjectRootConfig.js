1e17d174eb3ee2e4a1f2e30f19e77bedd25398c7
Jordan Harband <ljharb@gmail.com>
2018-07-11 00:10:46 -0700

    [Fix] `getProjectRootConfig`: return the normalized config

--- src/helpers/getProjectRootConfig.js
+++ src/helpers/getProjectRootConfig.js
@@ -2,10 +2,11 @@ import path from 'path';
 import fs from 'fs';
 import yargs from 'yargs';
 
+import normalizeConfig from './normalizeConfig';
 
 export default function getProjectRootConfig(projectRoot = process.cwd(), configPath = undefined) {
   const config = configPath
     ? JSON.parse(fs.readFileSync(path.join(projectRoot, configPath)))
     : yargs.pkgConf('react-component-variations', projectRoot).parse('');
-  throw config;
+  return normalizeConfig(config);
 }
